9c2e2384a71b61fd1376da703c5cbec3ec0f84de
chlohal <coleh@coleh.net>
2022-12-04 15:43:28 -0500

    rm lexer to prevent merge issues when mai pushes her code

--- src/parser/lexer.py
+++
@@ -1,9 +0,0 @@
-from structures.Token import Token
-
-def lexer(content: str) -> list[Token]:
-    i = 0
-    while i < len(content):
-        if content.startswith(",", i):
-            yield Token("comma", ",")
-            i += len(",")
-        elif content.startswith("=", i)
