2664d3af798decba67efbe6df190d20ee0d80cfc
chlohal <coleh@coleh.net>
2022-12-06 23:26:19 -0500

    add Mai's token names

--- src/parser/parsing/parse_statement.py
+++ src/parser/parsing/parse_statement.py
@@ -8,13 +8,13 @@ from parser.structures.syntax.statements.LetStatement import LetStatement
 def parse_statement(tokens: TokenStream):
     next_token = tokens.peek()
     
-    if next_token.name == "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_DEF_TOKEN":
+    if next_token.name == "def":
         return parse_def_statement(tokens)
-    elif next_token.name == "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_LET_TOKEN":
+    elif next_token.name == "let":
         return parse_let_statement(tokens)
-    elif next_token.name == "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_SOLVE_TOKEN":
+    elif next_token.name == "solve":
         return parse_solve_statement(tokens)
-    elif next_token.name == "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_EVAL_TOKEN":
+    elif next_token.name == "eval":
         return parse_eval_statement(tokens)
     else:
         return parse_eval_statement(tokens)
@@ -29,19 +29,19 @@ def parse_def_statement(tokens: TokenStream):
 
 def parse_let_statement(tokens: TokenStream):
     # expect a let, throw it away
-    expect(tokens, "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_LET_TOKEN")
+    expect(tokens, "let")
     
     # expect a variable token & wrap into a Variable. 
     # If we don't get a variable, complain about it in a descriptive way
     variable = Variable(
-        expect(tokens, "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_VARIABLE_TOKEN", """
+        expect(tokens, "var", """
         A variable definition should follow the format `let variable = expression`.
         Variables must ONLY contain letters and underscores. This variable was parsed as '{got_content}', which is a {got}. 
     """).content
     )
     
     # expect an equals sign. Throw it away. If we get something else, complain.
-    expect(tokens, "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_EQUALS_TOKEN", """ 
+    expect(tokens, "equal", """ 
         A variable definition should follow the format `let variable = expression`. Instead of an equals sign, the parser found a {got}.
     """)
     
@@ -52,7 +52,7 @@ def parse_let_statement(tokens: TokenStream):
 
 def parse_eval_statement(tokens: TokenStream):
     # Get rid of the `eval` token, if it's there. An expression by its own is interpreted as an eval statement.
-    if(tokens.peek().name == "REPLACE_LATER_WHATEVER_MAI_CHOSES_FOR_EVAL_TOKEN"):
+    if(tokens.peek().name == "eval"):
         tokens.pop()
         
     expr = parse_expression(tokens)
