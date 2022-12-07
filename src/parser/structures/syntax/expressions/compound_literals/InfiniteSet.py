3042634b3ed02bc815d50009d9d3b42e5d8e8676
chlohal <coleh@coleh.net>
2022-12-06 23:36:04 -0500

    infinite set evaluation. i hope

--- src/parser/structures/syntax/expressions/compound_literals/InfiniteSet.py
+++ src/parser/structures/syntax/expressions/compound_literals/InfiniteSet.py
@@ -22,8 +22,22 @@ class InfiniteSet(Expression):
 
     def evaluate(self, vars: EvaluationVariables) -> EvaluationResult:
         
-        condition_evaluation = vars.copy_without_specific(self.boundVariable.name)
+        condition_evaluation_context = vars.copy_without_specific(self.boundVariable.name)
         
-        return EvaluationResult(EvaluationResultType.FINITE_SET, 
+        conditions_evaluated = set()
+        
+        for c in self.conditions:
+            # left must be a variable, as confirmed by check_all_lhs. Don't care about it.
+            
+            rhs: Expression = c.right
+            
+            evald = c.copy()
+            evald.rhs = rhs.evaluate(condition_evaluation_context)
+            
+            conditions_evaluated.add(evald)
+            
+        
+        
+        return EvaluationResult(EvaluationResultType.INFINITE_SET, 
             set([v.evaluate(vars) for v in self.values])
         )
