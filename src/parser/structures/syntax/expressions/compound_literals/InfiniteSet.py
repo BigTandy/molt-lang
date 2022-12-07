b22b8a0d744380d3d31dc2bc1eb34f8f05711e80
chlohal <coleh@coleh.net>
2022-12-06 23:30:13 -0500

    make infinite sets check that they're based on their bound variable

--- src/parser/structures/syntax/expressions/compound_literals/InfiniteSet.py
+++ src/parser/structures/syntax/expressions/compound_literals/InfiniteSet.py
@@ -6,9 +6,19 @@ from parser.structures.syntax.expressions.base_literals.Variable import Variable
 
 
 class InfiniteSet(Expression):
-    def __init__(self, boundVariable: Variable, conditions: list[Condition]) -> None:
+    def __init__(self, boundVariable: Variable, conditions: set[Condition]) -> None:
         self.boundVariable = boundVariable
         self.conditions = conditions
+
+        self.check_all_lhs()
+        
+    def check_all_lhs(self):
+        for condition in self.conditions:
+            if(type(condition.left) != Variable or condition.left.name != self.boundVariable.name):
+                raise Exception("""
+                Conditions in infinite sets MUST directly depend on the set's bound variable; i.e. `{ x | x < 3 }` is legal, but `{ x | 3x > x }` or `{ x | 3 > x }` is not.
+                Please reword your conditions to directly depend on the set's bound variable.
+                """)
 
     def evaluate(self, vars: EvaluationVariables) -> EvaluationResult:
         
