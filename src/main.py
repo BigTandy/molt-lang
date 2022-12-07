5c5e33de03bc2e46f41cffc3a9db2ef55d0d5736
TippyToo <wm.titus2@gmail.com>
2022-12-07 12:27:15 -0500

    -Error if specified path doesn't exist -If no path is specified, use current working directory -If specified path is a file, search for 'main.molt' -Error is not found -Error if path is not file or directory

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from parser.lexer import lexer
 from parser.parsing.parse_file import parse_file
 from parser.parsing.token_stream import TokenStream
@@ -13,13 +14,36 @@ from parser.parsing.token_stream import TokenStream
 # use parse_file() to get a structure representing the file
 # call .run() on that object
 
-filename = sys.argv[2]
+# If no path is specified, use current working directory
+if len(sys.argv) < 3:
+    fpath = os.getcwd()
+else:
+    fpath = sys.argv[2]
 
-with open(filename, mode='r') as file:
-    source_code = file.read()
+# If specified path does not exist, raise exception
+if not os.path.exists(fpath):
+    raise Exception(f'Specified path {fpath} does not exist')
 
-    token_stream = TokenStream(lexer(source_code))
+def run_file(path: str):
+    with open(path, mode='r') as file:
+        source_code = file.read()
 
-    molt_file = parse_file(token_stream)
+        token_stream = TokenStream(lexer(source_code))
 
-    molt_file.run()
+        molt_file = parse_file(token_stream)
+
+        molt_file.run()
+
+# If specified path is a file, run it
+if os.path.isfile(fpath):
+    run_file(fpath)
+# If specified path is a dir, search it for 'main.molt', and raise exception if none is found
+elif os.path.isdir(fpath):
+    file_list = os.listdir(fpath)
+
+    if 'main.molt' in file_list:
+        run_file(os.path.join(fpath, 'main.molt'))
+    else:
+        raise Exception(f"{fpath} is a directory and no file 'main.molt' found")
+else:
+    raise Exception(f'{fpath} is not a valid file or directory')
